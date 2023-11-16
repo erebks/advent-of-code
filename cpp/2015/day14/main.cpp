40024beb5d47f86aae1f47471b46d36c6f6c3615
Lukas Vogl <l.vogl@viewpointsystem.com>
2023-11-16 16:57:13 +0100

    Solved 2015 day 14

--- cpp/2015/day14/main.cpp
+++ cpp/2015/day14/main.cpp
@@ -15,11 +15,29 @@ std::vector<std::string> resplit(const std::string &s, const std::regex &sep_reg
     return {iter, end};
 }
 
+int get_distance(std::string name, std::array<int, 4> dat, int time) {
+    int cycle_time = dat[1] + dat[2];
+
+    int tot_cycles = (int)time / cycle_time;
+    int remainder = (int)time % cycle_time;
+    int distance = 0;
+
+    distance = tot_cycles * dat[1] * dat[0];
+
+    if (remainder <= dat[1]) {
+        distance += remainder * dat[0];
+    } else {
+        distance += dat[1] * dat[0];
+    }
+
+    return distance;
+}
+
 int main(void) {
     std::ifstream text_input("../day14.txt");
     std::string input;
 
-    std::map<std::string, std::array<int, 3>> reindeers;
+    std::map<std::string, std::array<int, 4>> reindeers;
 
     if (text_input.is_open()) {
         while (std::getline(text_input, input)) {
@@ -50,23 +68,41 @@ int main(void) {
     int race_time = 2503;
 
     for (auto &reindeer : reindeers) {
-        int cycle_time = reindeer.second[1] + reindeer.second[2];
-
-        int tot_cycles = (int)race_time / cycle_time;
-        int remainder = (int)race_time % cycle_time;
-        int distance = 0;
-
-        if (remainder < reindeer.second[2]) {
-            distance = (tot_cycles + 1) * reindeer.second[1] * reindeer.second[0];
-        } else {
-            distance = tot_cycles * reindeer.second[1] * reindeer.second[0];
-        }
+        int distance = get_distance(reindeer.first, reindeer.second, race_time);
 
         distances.insert(distance);
-        std::cout << "After: " << race_time << "s, " << reindeer.first << " traveled: " << distance << std::endl;
+        // std::cout << "After: " << race_time << "s, " << reindeer.first << " traveled: " << distance << std::endl;
     }
 
     std::cout << "Part 1:\nAnswer: " << *distances.rbegin() << std::endl << std::endl;
 
-    // 2720 too high
+    int winning_distance = 0;
+
+    for (int time = 1; time <= race_time; time++) {
+        std::map<std::string, int> distances;
+        int max_distance = 0;
+        for (auto &reindeer : reindeers) {
+            int distance = get_distance(reindeer.first, reindeer.second, time);
+
+            distances.insert({reindeer.first, distance});
+            // std::cout << "After: " << time << "s, " << reindeer.first << " traveled: " << distance << std::endl;
+            if (distance > max_distance) {
+                max_distance = distance;
+            }
+        }
+
+        // Iterate over all reindeers and assign points
+        for (auto &dist : distances) {
+            if (dist.second == max_distance) {
+                // std::cout << "At: " << time << "s " << dist.first << " wins and gets a point" << std::endl;
+                auto r = reindeers.find(dist.first);
+                r->second[3]++;
+                if (r->second[3] > winning_distance) {
+                    winning_distance = r->second[3];
+                }
+            }
+        }
+    }
+
+    std::cout << "Part 2:\nAnswer: " << winning_distance << std::endl << std::endl;
 }
