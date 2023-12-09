2ba59f2df4215881a95b00de5f4a3ec8334b276d
Lukas Vogl <l.vogl1996@gmx.at>
2023-12-09 13:18:09 +0100

    Updated template

--- cpp/template/main.cpp
+++ cpp/template/main.cpp
@@ -2,6 +2,16 @@
 #include <iostream>
 #include <string>
 
+// Hat-tip to https://stackoverflow.com/a/28142357
+#include <regex>
+#include <type_traits>
+#include <vector>
+std::vector<std::string> resplit(const std::string &s, const std::regex &sep_regex = std::regex{"\\s+"}) {
+    std::sregex_token_iterator iter(s.begin(), s.end(), sep_regex, -1);
+    std::sregex_token_iterator end;
+    return {iter, end};
+}
+
 int main(void) {
     std::ifstream text_input("../dayXX.txt");
     std::string input;
