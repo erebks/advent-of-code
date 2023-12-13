e989afff63bd585e135f9ed7d1eb3b885d59ef74
Lukas Vogl <l.vogl1996@gmx.at>
2023-12-13 11:07:54 +0100

    Solved 2023 day 12 part 1

--- cpp/2023/day12/main.cpp
+++ cpp/2023/day12/main.cpp
@@ -23,49 +23,36 @@ void parse(std::string str, std::string &springs, std::vector<int> &groups) {
     }
 }
 
-bool is_arrangement_possible(std::string springs, std::string arrangement) {
-    if (springs.size() != arrangement.size()) {
-        std::cout << "Sizes don't match!" << std::endl;
-        return false;
-    }
+bool is_arrangement_valid(std::string arrangement, std::regex re) { return std::regex_match(arrangement, re); }
 
-    for (int i = 0; i < springs.size(); i++) {
-        std::cout << springs[i] << " vs. " << arrangement[i] << std::endl;
-        if ((springs[i] == '#' || springs[i] == '.') && arrangement[i] != springs[i]) {
-            std::cout << "\tError in " << i << "th digit" << std::endl;
-            return false;
-        }
-    }
-    return true;
-}
-
-std::vector<std::string> generate_arrangement(std::string background, std::vector<std::string> groups, int index) {
+std::vector<std::string> recursive(std::string fixed, std::vector<int> unknowns) {
     std::vector<std::string> arrangements;
 
-    std::cout << "Background: " << background << " Index: " << index << std::endl;
-    // Base case
-    if (groups.size() == 0) {
-        arrangements.push_back(background);
+    if (unknowns.size() == 0) {
+        arrangements.push_back(fixed);
         return arrangements;
     }
-    // Take next group
-    std::string grp = groups.back();
-    groups.pop_back();
 
-    // Calculate how this group can be shifted
-    std::string prefix;
-    for (auto g : groups) {
-        prefix += '.' + g;
+    // Next unknown
+    int idx = unknowns.back();
+    unknowns.pop_back();
+
+    std::vector<std::string> a;
+
+    fixed[idx] = '.';
+    a = recursive(fixed, unknowns);
+
+    for (auto b : a) {
+        arrangements.push_back(b);
     }
 
-    for (int i = index; i < background.size() - prefix.size(); i++) {
-        std::string arrangement = background.substr(0, i);
-        arrangement += grp + '.';
-        std::vector<std::string> a = generate_arrangement(arrangement, groups, arrangement.size() - 1);
-        for (auto b : a) {
-            arrangements.push_back(b);
-        }
+    fixed[idx] = '#';
+    a = recursive(fixed, unknowns);
+
+    for (auto b : a) {
+        arrangements.push_back(b);
     }
+
     return arrangements;
 }
 
@@ -73,28 +60,38 @@ int arrangements(std::string springs, std::vector<int> groups) {
     // Groups are seperated by '.' and consist only of '#'
     // '?' are either '.' or '#'
 
-    std::string background;
-    for (auto s : springs) {
-        background += '.';
-    }
+    std::vector<int> unknowns;
 
-    std::vector<std::string> spring_groups;
-
-    for (int i1 = groups.size() - 1; i1 >= 0; i1++) {
-        std::string grp;
-        for (int i = 0; i < groups[i1]; i++) {
-            grp += '#';
+    for (int i = 0; i < springs.size(); i++) {
+        if (springs[i] == '?') {
+            unknowns.push_back(i);
         }
-        spring_groups.push_back(grp);
     }
 
-    std::vector<std::string> arrangements = generate_arrangement(background, spring_groups, 0);
+    std::vector<std::string> arrangements = recursive(springs, unknowns);
+    int cnt = 0;
+
+    std::string re = "\\.*";
+
+    for (auto g : groups) {
+        re += "#{" + std::to_string(g) + "}\\.+";
+    }
+
+    // Delete .+ at the back
+    re = re.substr(0, re.size() - 3);
+    re += "\\.*";
 
     for (auto a : arrangements) {
-        std::cout << a << std::endl;
+        // std::cout << "Arrangement: '" << a << "' ";
+        if (is_arrangement_valid(a, std::regex(re))) {
+            // std::cout << "VALID" << std::endl;
+            cnt++;
+        } else {
+            // std::cout << "NOT VALID" << std::endl;
+        }
     }
 
-    return arrangements.size();
+    return cnt;
 }
 
 int main(void) {
@@ -115,16 +112,18 @@ int main(void) {
         return -1;
     }
 
-    str = {
-        "???.### 1,1,3", ".??..??...?##. 1,1,3", "?#?#?#?#?#?#?#? 1,3,1,6", "????.#...#... 4,1,1", "????.######..#####. 1,6,5", "?###???????? 3,2,1",
-    };
-    std::string springs;
-    std::vector<int> groups;
+    // str = {
+    //     "???.### 1,1,3", ".??..??...?##. 1,1,3", "?#?#?#?#?#?#?#? 1,3,1,6", "????.#...#... 4,1,1", "????.######..#####. 1,6,5", "?###???????? 3,2,1",
+    // };
 
+    int p1 = 0;
     for (auto s : str) {
+        std::string springs;
+        std::vector<int> groups;
+
         parse(s, springs, groups);
-        // arrangements(springs, groups);
+        p1 += arrangements(springs, groups);
     }
 
-    arrangements(springs, groups);
+    std::cout << "Part 1:\nAnswer: " << p1 << std::endl << std::endl;
 }
