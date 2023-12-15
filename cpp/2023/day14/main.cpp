157b3338840924547dd60562359cb100c6287da0
Lukas Vogl <l.vogl@viewpointsystem.com>
2023-12-15 08:52:53 +0100

    Solved 2023 day 15

--- cpp/2023/day14/main.cpp
+++ cpp/2023/day14/main.cpp
@@ -69,19 +69,50 @@ std::vector<std::string> tilt(std::vector<std::string> platform, char axis = 'N'
         transonded = transponse(transonded);
 
         for (auto s : transonded) {
-            std::cout << s << " -> ";
+            // std::cout << s << " -> ";
             p.push_back(roll_left(s));
-            std::cout << p.back() << std::endl;
+            // std::cout << p.back() << std::endl;
         }
 
         p = transponse(p);
 
         break;
     case 'E':
+        transonded = transponse(platform);
+        transonded = transponse(transonded);
+
+        for (auto s : transonded) {
+            // std::cout << s << " -> ";
+            p.push_back(roll_left(s));
+            // std::cout << p.back() << std::endl;
+        }
+
+        p = transponse(p);
+        p = transponse(p);
+
         break;
     case 'S':
+        transonded = transponse(platform);
+
+        for (auto s : transonded) {
+            std::cout << s << " -> ";
+            p.push_back(roll_left(s));
+            std::cout << p.back() << std::endl;
+        }
+
+        p = transponse(p);
+        p = transponse(p);
+        p = transponse(p);
+
         break;
     case 'W':
+
+        for (auto s : platform) {
+            // std::cout << s << " -> ";
+            p.push_back(roll_left(s));
+            // std::cout << p.back() << std::endl;
+        }
+
         break;
     default:
         break;
@@ -103,6 +134,24 @@ int load(std::vector<std::string> platform) {
     return l;
 }
 
+bool platform_is_equal(std::vector<std::string> a, std::vector<std::string> b) {
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] != b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int platform_is_equal(std::vector<std::string> a, std::vector<std::vector<std::string>> b) {
+    for (int i = 0; i < b.size(); i++) {
+        if (platform_is_equal(a, b[i])) {
+            return i;
+        }
+    }
+    return -1;
+}
+
 int main(void) {
     std::ifstream text_input("../day14.txt");
     std::string input;
@@ -120,11 +169,76 @@ int main(void) {
         return -1;
     }
 
-    // platform = {
-    //     "O....#....", "O.OO#....#", ".....##...", "OO.#O....O", ".O.....O#.", "O.#..O.#.#", "..O..#O..O", ".......O..", "#....###..", "#OO..#....",
-    // };
+    platform = {
+        "O....#....", "O.OO#....#", ".....##...", "OO.#O....O", ".O.....O#.", "O.#..O.#.#", "..O..#O..O", ".......O..", "#....###..", "#OO..#....",
+    };
 
-    std::vector<std::string> tilted = tilt(platform);
+    std::vector<std::string> tilted(platform);
+
+    std::vector<std::vector<std::string>> platforms_so_far;
+    // platforms_so_far.push_back(platform);
+
+    //    for (unsigned long i = 0; i < 1000000000; i++) {
+    for (unsigned long i = 0; i < 1; i++) {
+        int idx = 0;
+        tilted = tilt(tilted, 'N');
+        idx = platform_is_equal(tilted, platforms_so_far);
+        if (idx > 0) {
+            std::cout << "Tilted North -> Platform " << idx << " equal!" << std::endl;
+        }
+        platforms_so_far.push_back(tilted);
+
+        std::cout << std::endl;
+        for (auto line : tilted) {
+            std::cout << line << std::endl;
+        }
+
+        tilted = tilt(tilted, 'W');
+        idx = platform_is_equal(tilted, platforms_so_far);
+        if (idx > 0) {
+            std::cout << "Tilted West -> Platform " << idx << " equal!" << std::endl;
+        }
+        platforms_so_far.push_back(tilted);
+        std::cout << std::endl;
+        for (auto line : tilted) {
+            std::cout << line << std::endl;
+        }
+
+        tilted = tilt(tilted, 'S');
+        idx = platform_is_equal(tilted, platforms_so_far);
+        if (idx > 0) {
+            std::cout << "Tilted South -> Platform " << idx << " equal!" << std::endl;
+        }
+        platforms_so_far.push_back(tilted);
+        std::cout << std::endl;
+        for (auto line : tilted) {
+            std::cout << line << std::endl;
+        }
+
+        tilted = tilt(tilted, 'E');
+        idx = platform_is_equal(tilted, platforms_so_far);
+        if (idx > 0) {
+            std::cout << "Tilted East -> Platform " << idx << " equal!" << std::endl;
+        }
+        platforms_so_far.push_back(tilted);
+        std::cout << std::endl;
+        for (auto line : tilted) {
+            std::cout << line << std::endl;
+        }
+
+        std::cout << "Iteration: " << i << std::endl;
+    }
+
+    std::cout << std::endl;
+    for (auto line : platforms_so_far[4]) {
+        std::cout << line << std::endl;
+    }
+
+    std::cout << std::endl;
+    for (auto line : platforms_so_far[8]) {
+        std::cout << line << std::endl;
+    }
+
     int l = load(tilted);
 
     std::cout << "Part 1\nAnswer: " << l << std::endl << std::endl;
